194d207db12f1d5149219bd9003c32e5336dc2f5
unknown <raffipasya4826@gmail.com>
2025-06-20 16:36:07 +0700

    menambahkan file code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 javascript
 /**@type{import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // atau 'media'
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
